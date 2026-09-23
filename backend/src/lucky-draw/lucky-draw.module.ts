942a7795286823fbfed809ecfe50e76f75edc24f
Prashant Shukla <shivi.shukla.tanya@gmail.com>
2026-09-23 22:39:18 +0530

    feat: register lucky draw fulfillment service

--- backend/src/lucky-draw/lucky-draw.module.ts
+++ backend/src/lucky-draw/lucky-draw.module.ts
@@ -1,14 +1,28 @@
 import { Module } from '@nestjs/common';
 import {
   LuckyDrawExecutionController,
+  LuckyDrawFulfillmentController,
   LuckyDrawPolicyController,
 } from './lucky-draw.controller';
 import { LuckyDrawExecutionService } from './lucky-draw-execution.service';
+import { LuckyDrawFulfillmentService } from './lucky-draw-fulfillment.service';
 import { LuckyDrawPolicyService } from './lucky-draw-policy.service';
 
 @Module({
-  controllers: [LuckyDrawPolicyController, LuckyDrawExecutionController],
-  providers: [LuckyDrawPolicyService, LuckyDrawExecutionService],
-  exports: [LuckyDrawPolicyService, LuckyDrawExecutionService],
+  controllers: [
+    LuckyDrawPolicyController,
+    LuckyDrawExecutionController,
+    LuckyDrawFulfillmentController,
+  ],
+  providers: [
+    LuckyDrawPolicyService,
+    LuckyDrawExecutionService,
+    LuckyDrawFulfillmentService,
+  ],
+  exports: [
+    LuckyDrawPolicyService,
+    LuckyDrawExecutionService,
+    LuckyDrawFulfillmentService,
+  ],
 })
 export class LuckyDrawModule {}
