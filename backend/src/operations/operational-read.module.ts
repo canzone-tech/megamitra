fbedb908a3d9195d58029b7a1bfb78e06f9b9b00
Prashant Shukla <shivi.shukla.tanya@gmail.com>
2026-09-23 22:59:38 +0530

    fix: register operational response interceptor

--- backend/src/operations/operational-read.module.ts
+++ backend/src/operations/operational-read.module.ts
@@ -3,11 +3,12 @@ import {
   AdminOperationalReadController,
   MemberOperationalReadController,
 } from './operational-read.controller';
+import { OperationalJsonSafeInterceptor } from './operational-json-safe.interceptor';
 import { OperationalReadService } from './operational-read.service';
 
 @Module({
   controllers: [MemberOperationalReadController, AdminOperationalReadController],
-  providers: [OperationalReadService],
+  providers: [OperationalReadService, OperationalJsonSafeInterceptor],
   exports: [OperationalReadService],
 })
 export class OperationalReadModule {}
