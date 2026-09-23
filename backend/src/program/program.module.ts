62754d0a90b4b54a13118c536b04ee7180b16e91
Prashant Shukla <shivi.shukla.tanya@gmail.com>
2026-09-23 21:35:08 +0530

    feat: wire program event orchestration service

--- backend/src/program/program.module.ts
+++ backend/src/program/program.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { BinaryUnitModule } from '../binary-unit/binary-unit.module';
 import {
   ProgramEnrollmentController,
   ProgramPaymentController,
@@ -6,22 +7,32 @@ import {
 } from './program.controller';
 import { ProgramEligibilityService } from './program-eligibility.service';
 import { ProgramEnrollmentService } from './program-enrollment.service';
+import { ProgramOrchestrationController } from './program-orchestration.controller';
+import { ProgramOrchestrationService } from './program-orchestration.service';
 import { ProgramPaymentService } from './program-payment.service';
 import { ProgramPolicyService } from './program-policy.service';
 
 @Module({
-  controllers: [ProgramPolicyController, ProgramEnrollmentController, ProgramPaymentController],
+  imports: [BinaryUnitModule],
+  controllers: [
+    ProgramPolicyController,
+    ProgramEnrollmentController,
+    ProgramPaymentController,
+    ProgramOrchestrationController,
+  ],
   providers: [
     ProgramEligibilityService,
     ProgramPolicyService,
     ProgramEnrollmentService,
     ProgramPaymentService,
+    ProgramOrchestrationService,
   ],
   exports: [
     ProgramEligibilityService,
     ProgramPolicyService,
     ProgramEnrollmentService,
     ProgramPaymentService,
+    ProgramOrchestrationService,
   ],
 })
 export class ProgramModule {}
