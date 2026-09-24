858ea1c6346c72c3d4662640f835e53f8a66d279
Prashant Shukla <shivi.shukla.tanya@gmail.com>
2026-09-24 10:05:05 +0530

    feat: wire auth recovery email services

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,14 +2,25 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { CaptchaModule } from '../captcha/captcha.module';
 import { AuthController } from './auth.controller';
+import { AuthEmailTemplateController } from './auth-email-template.controller';
+import { AuthEmailTemplateService } from './auth-email-template.service';
+import { AuthRecoveryService } from './auth-recovery.service';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { PasswordService } from './password.service';
+import { SmtpMailService } from './smtp-mail.service';
 
 @Module({
   imports: [JwtModule.register({}), CaptchaModule],
-  controllers: [AuthController],
-  providers: [AuthService, PasswordService, JwtAuthGuard],
-  exports: [PasswordService, JwtAuthGuard],
+  controllers: [AuthController, AuthEmailTemplateController],
+  providers: [
+    AuthService,
+    AuthRecoveryService,
+    AuthEmailTemplateService,
+    SmtpMailService,
+    PasswordService,
+    JwtAuthGuard,
+  ],
+  exports: [PasswordService, JwtAuthGuard, AuthRecoveryService],
 })
 export class AuthModule {}
