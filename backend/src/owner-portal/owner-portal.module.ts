435ca4212e96355364af4bb7f6c68c777b657bb4
Prashant Shukla <shivi.shukla.tanya@gmail.com>
2026-09-25 18:43:51 +0530

    feat: register owner management portal module

---
+++ backend/src/owner-portal/owner-portal.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { BinaryPolicyModule } from '../binary-policy/binary-policy.module';
+import { GenealogyModule } from '../genealogy/genealogy.module';
+import { LedgerModule } from '../ledger/ledger.module';
+import { LuckyDrawModule } from '../lucky-draw/lucky-draw.module';
+import { ProgramModule } from '../program/program.module';
+import { ReferralRewardModule } from '../referral-reward/referral-reward.module';
+import { UsersModule } from '../users/users.module';
+import { OwnerPortalController } from './owner-portal.controller';
+import { OwnerPortalService } from './owner-portal.service';
+
+@Module({
+  imports: [
+    UsersModule,
+    GenealogyModule,
+    ProgramModule,
+    BinaryPolicyModule,
+    ReferralRewardModule,
+    LuckyDrawModule,
+    LedgerModule,
+  ],
+  controllers: [OwnerPortalController],
+  providers: [OwnerPortalService],
+  exports: [OwnerPortalService],
+})
+export class OwnerPortalModule {}
