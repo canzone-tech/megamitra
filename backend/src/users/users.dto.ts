e591a16e98b8e13fac8ec6c2990fbaaefc85eca0
Prashant Shukla <shivi.shukla.tanya@gmail.com>
2026-09-25 18:35:40 +0530

    feat: support managed member creation

--- backend/src/users/users.dto.ts
+++ backend/src/users/users.dto.ts
@@ -1,5 +1,45 @@
-import { ArrayMinSize, IsArray, IsEnum, IsString } from 'class-validator';
+import {
+  ArrayMinSize,
+  IsArray,
+  IsEmail,
+  IsEnum,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 import { UserStatus } from '../generated/prisma/enums';
+
+export class CreateManagedUserDto {
+  @IsOptional()
+  @IsString()
+  @MinLength(3)
+  @MaxLength(191)
+  username?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(50)
+  phone?: string;
+
+  @IsString()
+  @MinLength(1)
+  password!: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  firstName?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  lastName?: string;
+}
 
 export class UpdateUserStatusDto {
   @IsEnum(UserStatus)
